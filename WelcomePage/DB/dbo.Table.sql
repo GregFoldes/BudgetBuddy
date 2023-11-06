649d4472fc2066877d7d5b7fa551e69265fe7d84
Greg Foldes <142818437+GregFoldes@users.noreply.github.com>
2023-11-06 11:52:46 -0500

    nininiinniinnini

---
+++ WelcomePage/DB/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE PersonalFinance (
+    TransactionID INT  PRIMARY KEY,
+    Category VARCHAR(50) NOT NULL,
+    Description VARCHAR(255),
+    Amount DECIMAL(10, 2) NOT NULL,
+    Date DATE NOT NULL,
+    Notes TEXT,
+    CONSTRAINT chk_amount_non_negative CHECK (Amount >= 0)
+);
---
+++ WelcomePage/DB/dbo.Table_1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE UserInformation (
+    UserID INT PRIMARY KEY,
+    Username VARCHAR(50) NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    PhoneNumber VARCHAR(15)
+);
